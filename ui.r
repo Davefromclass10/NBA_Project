c226a74e39fbbb895cab482b922c61ccec5db621
David Strahm <daveystrahm10@gmail.com>
2020-05-14 19:29:46 -0600

    maybe

--- ui.r
+++ ui.r
@@ -8,14 +8,8 @@ shiny::shinyUI(pageWithSidebar(
   headerPanel('Denver Nuggets Post Game Breakdown'),
   
   sidebarPanel(
-    fileInput('Player_BoxScores2020', "Choose CSV File",
-              accept = c(
-                "text/csv",
-                "test/comma-separated-values,text/plain",
-                ".csv")
-              ),
-              tags$hr(),
-              selectInput('variable', 'Team:', TRUE)
+    selectInput('variable', 'Team:',
+                list(unique(Player_BoxScores2020$slugTeam)))
               
   ),
   
