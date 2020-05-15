85d87734dbad85db1f2066f47538ff50ca037015
David Strahm <daveystrahm10@gmail.com>
2020-05-14 19:49:58 -0600

    132

--- ui.r
+++ ui.r
@@ -1,5 +1,9 @@
 library(shiny)
 
+
+PlayerBoxScores <- read.csv(file = 'Player_BoxScores2020.csv')
+
+Player_Traditional <- PlayerBoxScores[,c("obs","idGame","slugTeam","namePlayer","idPlayer","groupStartPosition","fgm","fga","pctFG","fg3m","fg3a","pctFG3","ftm","fta","pctFT","oreb","dreb","treb","ast","tov","stl","blk","pf","pts","plusminus")]
 #Define UI
 
 shiny::shinyUI(pageWithSidebar(
