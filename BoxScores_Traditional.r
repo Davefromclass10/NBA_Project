fb92d4ffb177bd354ea8476721e6e5672bfe3aaf
David Strahm <daveystrahm10@gmail.com>
2020-05-19 15:04:17 -0600

    Traditional Box scores for 2020

---
+++ BoxScores_Traditional.r
@@ -0,0 +1,3 @@
+BoxScores2020 <- read.csv("C:/Users/stram/Documents/Elements/NBA_Data/Player_BoxScores2020.csv")
+BoxScores_Traditional2020 <- BoxScores_2020[,c("X","idGame","slugTeam","namePlayer","idPlayer","groupStartPosition","fgm","fga","pctFG","fg3m","fg3a","pctFG3","ftm","fta","pctFT","oreb","dreb","treb","ast","tov","stl","blk","pf","pts","plusminus")]
+
